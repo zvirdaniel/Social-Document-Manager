9fecf9dfd379f9845769f06e2a2f303ac5a2abe1
Daniel Zvir <zvir.daniel@seznam.cz>
2020-10-27 17:02:14 +0100

    Added more integration tests, configured swagger

--- src/test/java/cz/zvir/social/IntegrationTests.java
+++ src/test/java/cz/zvir/social/IntegrationTests.java
@@ -1,7 +1,10 @@
 package cz.zvir.social;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.zvir.social.models.Document;
+import cz.zvir.social.models.Like;
 import cz.zvir.social.models.User;
+import cz.zvir.social.repositories.DocumentRepository;
 import cz.zvir.social.repositories.UserRepository;
 import cz.zvir.social.requests.DocumentRequest;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,15 +19,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Testcontainers
@@ -34,6 +39,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class IntegrationTests {
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private DocumentRepository documentRepository;
 
 	@Autowired
 	private WebApplicationContext wac;
@@ -67,13 +75,14 @@ public class IntegrationTests {
 		this.mockMvc.perform(MockMvcRequestBuilders.post("/users").param("name", name))
 		            .andDo(print())
 		            .andExpect(status().isOk())
-		            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name));
+		            .andExpect(jsonPath("$.name").value(name));
 	}
 
 	@Test
 	public void deleteUserTest() throws Exception {
 		final User josef = userRepository.save(new User("Josef"));
 		this.mockMvc.perform(MockMvcRequestBuilders.delete("/users/id/" + josef.getId()))
+		            .andDo(print())
 		            .andExpect(status().isOk());
 		assertFalse(userRepository.existsById(josef.getId()));
 	}
@@ -85,26 +94,131 @@ public class IntegrationTests {
 		this.mockMvc.perform(MockMvcRequestBuilders.patch("/users/id/" + josef.getId()).param("name", newName))
 		            .andDo(print())
 		            .andExpect(status().isOk())
-		            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(newName));
+		            .andExpect(jsonPath("$.name").value(newName));
 	}
 
 	@Test
 	public void createDocumentTest() throws Exception {
 		final String docId = "ImportantArticle";
 		final String content = "some text content";
-		final User user = this.getFreshUser();
+		final User user = this.getNewUser();
 		this.mockMvc.perform(MockMvcRequestBuilders.post("/documents/id/" + docId)
 		                                           .contentType(MediaType.APPLICATION_JSON)
 		                                           .content(asJsonString(new DocumentRequest(content, user.getId()))))
 		            .andDo(print())
 		            .andExpect(status().isOk())
-		            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(docId))
-		            .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(content))
-		            .andExpect(MockMvcResultMatchers.jsonPath("$.user.id").value(user.getId()));
+		            .andExpect(jsonPath("$.id").value(docId))
+		            .andExpect(jsonPath("$.content").value(content))
+		            .andExpect(jsonPath("$.user.id").value(user.getId()));
 	}
 
-	private User getFreshUser() {
-		return userRepository.saveAndFlush(new User("Karel " + System.currentTimeMillis()));
+	@Test
+	public void likeDocumentTest() throws Exception {
+		final Document document = this.getNewDocument("docID", "content", null);
+		final User likerOne = this.getNewUser();
+		final User likerTwo = this.getNewUser();
+		this.mockMvc.perform(MockMvcRequestBuilders.patch("/documents/like/" + document.getId())
+		                                           .param("userId", String.valueOf(likerOne.getId())))
+		            .andDo(print())
+		            .andExpect(status().isOk())
+		            .andExpect(jsonPath("$.id").value(document.getId()))
+		            .andExpect(jsonPath("$.content").value(document.getContent()))
+		            .andExpect(jsonPath("$.likes").isArray())
+		            .andExpect(jsonPath("$.likes[0]").isNotEmpty())
+		            .andExpect(jsonPath("$.likes[0].user.id").value(likerOne.getId()));
+
+		this.mockMvc.perform(MockMvcRequestBuilders.patch("/documents/like/" + document.getId())
+		                                           .param("userId", String.valueOf(likerTwo.getId())))
+		            .andDo(print())
+		            .andExpect(status().isOk())
+		            .andExpect(jsonPath("$.id").value(document.getId()))
+		            .andExpect(jsonPath("$.content").value(document.getContent()))
+		            .andExpect(jsonPath("$.likes").isArray())
+		            .andExpect(jsonPath("$.likes[1]").isNotEmpty())
+		            .andExpect(jsonPath("$.likes[1].user.id").value(likerTwo.getId()));
+	}
+
+	@Test
+	public void unlikeDocumentTest() throws Exception {
+		final User likerOne = this.getNewUser();
+		final Document document = this.likeDocument(this.getNewDocument("docId", "content", this.getNewUser()), likerOne);
+		this.mockMvc.perform(MockMvcRequestBuilders.patch("/documents/unlike/" + document.getId())
+		                                           .param("userId", String.valueOf(likerOne.getId())))
+		            .andDo(print())
+		            .andExpect(status().isOk())
+		            .andExpect(jsonPath("$.id").value(document.getId()))
+		            .andExpect(jsonPath("$.content").value(document.getContent()))
+		            .andExpect(jsonPath("$.likes").isArray())
+		            .andExpect(jsonPath("$.likes.length()").value(0));
+	}
+
+	@Test
+	public void documentLikeCountTest() throws Exception {
+		final Document document = this.likeDocument(this.getNewDocument("docId", "content", this.getNewUser()),
+		                                            this.getNewUser(), this.getNewUser(), this.getNewUser());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/documents/id/" + document.getId()))
+		            .andDo(print())
+		            .andExpect(status().isOk())
+		            .andExpect(jsonPath("$.id").value(document.getId()))
+		            .andExpect(jsonPath("$.content").value(document.getContent()))
+		            .andExpect(jsonPath("$.likeCount").value(3));
+	}
+
+	@Test
+	public void getLikedDocumentsTest() throws Exception {
+		// This user will like the documents
+		final User liker = this.getNewUser();
+
+		// Documents liked by the user, each document is created by new user (not by the liking user)
+		final Document[] likedDocuments = {
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), liker),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), liker),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), liker),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), liker),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), liker),
+		};
+
+		// Unrelated documents, each documents is created by new user and liked by another new user
+		final Document[] unlikedDocuments = {
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), this.getNewUser()),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), this.getNewUser()),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), this.getNewUser()),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), this.getNewUser()),
+				this.likeDocument(this.getNewDocument("doc" + System.currentTimeMillis(), "content", this.getNewUser()), this.getNewUser())
+		};
+
+		final var actions = this.mockMvc.perform(MockMvcRequestBuilders.get("/documents/liked-documents").param("userId", String.valueOf(liker.getId())))
+		                                .andDo(print())
+		                                .andExpect(status().isOk())
+		                                .andExpect(jsonPath("$").isArray())
+		                                .andExpect(jsonPath("$.length()").value(5));
+
+		// Check that liked documents are returned
+		for (final var likedDocument : likedDocuments) {
+			actions.andExpect(jsonPath("$[?(@.id == '" + likedDocument.getId() + "')]").exists());
+		}
+
+		// Check that unrelated documents are not returned
+		for (final var unlikedDocument : unlikedDocuments) {
+			actions.andExpect(jsonPath("$[?(@.id == '" + unlikedDocument.getId() + "')]").doesNotExist());
+		}
+	}
+
+	private Document likeDocument(final Document document, final User... users) {
+		if (users != null && users.length > 0) {
+			for (final var user : users) {
+				document.getLikes().add(new Like(document, user));
+			}
+		}
+		return documentRepository.saveAndFlush(document);
+	}
+
+	private Document getNewDocument(final String id, final String content, final User user) {
+		return documentRepository.saveAndFlush(new Document(id, content, Optional.ofNullable(user).orElseGet(this::getNewUser)));
+	}
+
+	private User getNewUser() {
+		return userRepository.saveAndFlush(new User("User " + System.currentTimeMillis()));
 	}
 
 	private String asJsonString(final Object obj) {
